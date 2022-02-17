99edcfb31e605645f0686af7878c1fd1752ce976
José Gustavo Oliveira da Silva <gustavo.myw@live.com>
2022-02-17 10:49:16 -0300

    Criação de componentes menu, cadastro, login e footer | edição de menu e cadastro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', // nome da tag que pode ser chamada diversas vezes no html
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    LoginComponent,
+    SignInComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
