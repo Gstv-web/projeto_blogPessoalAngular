d9753924c9638923b7a4727ba330c836257b80ed
José Gustavo Oliveira da Silva <gustavo.myw@live.com>
2022-02-18 15:12:37 -0300

    Inserção de Rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignInComponent } from './sign-in/sign-in.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", redirectTo: "login", pathMatch: "full"},
+  {path: "login", component: LoginComponent},
+  {path: "sign-in", component: SignInComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
