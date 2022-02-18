fcc76af5eeabe0d2ad50f0b1ea7968756eeb003d
José Gustavo Oliveira da Silva <gustavo.myw@live.com>
2022-02-18 10:56:31 -0300

    Implementação dos componentes login e footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
 const routes: Routes = [];
 
